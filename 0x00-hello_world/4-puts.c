9eaf78b8d4a5770fc21f08ac5db233d6f1fc53ff
root <alemenewendawek@gmail.com>
2022-03-10 05:33:58 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-  char hello[] = "Programming is like building a multilingual puzzle";
-  
-  puts(hello);
-  return (0);
+	char hello[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(hello);
+	return (0);
 }
