fe8661f406471520037ee66a4abd79a06f099f75
root <alemenewendawek@gmail.com>
2022-03-15 11:25:55 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,15 +9,6 @@
 
 int _islower(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (0);
-	}
+	return (c >= 97  && c <= 122);
 
-	else if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	_putchar ('\n');
-	return (0);
 }
