f385814d8f6d7f5d4cb7297251ba248522452a55
root <alemenewendawek@gmail.com>
2022-03-15 10:52:09 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,20 @@
 #include "main.h"
+/**
+ * _islower - Check description
+ * Description: It print the alphabet if it is upper 0 and if the lower 1
+ * @c: The character it hold the alphabet
+ * Return: Always 0 (Success)
+ */
+
 
 int _islower(int c)
 {
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (0);
 	}
 
-	else if(c >= 97 && c <= 122)
+	else if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
