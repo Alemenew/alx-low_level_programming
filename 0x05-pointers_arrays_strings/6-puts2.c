c184b3b7ac965c01b3120b3bc44110eaeab58b75
alemenew <alemenewendawek@gmail.com>
2022-03-22 02:03:37 +0300

    even

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i <= str[i];  i++)
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
