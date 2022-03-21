1effd0bd9f8d1351169373d8517978ac92851204
alemenew <alemenewendawek@gmail.com>
2022-03-21 20:34:37 +0300

    length of string

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -8,13 +8,11 @@
  */
 int main(void)
 {
-	int a;
-	int b;
-	a = 98;
-	b = 42;
+	char *str;
+	int len;
 
-	printf("a=%d, b=%d\n", a, b);
-	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b);
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
 	return (0);
 }
