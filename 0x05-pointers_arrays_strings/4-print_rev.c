7b68a6d008acb940fc6452a5a770926c28a8b337
alemenew <alemenewendawek@gmail.com>
2022-03-21 23:56:35 +0300

    revers the string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - print the revers of the string
+ *
+ * @s: hold all of the charactor
+ */
+
+void print_rev(char *s)
+
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	j = i - 1;
+
+	while (s[j])
+
+	{
+		j--;
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
