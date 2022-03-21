2ca83fedac6661d113ebbf47d1fddbbbcd4eaccb
alemenew <alemenewendawek@gmail.com>
2022-03-21 21:50:26 +0300

    printing string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
+ * _puts - print string
  *
- *
- *
+ *@str: hold string
  */
 
 void _puts(char *str)
 {
-	while (*str !='\0')
+	while (*str != '\0')
 	{
 		_putchar(*str);
 		str++;
