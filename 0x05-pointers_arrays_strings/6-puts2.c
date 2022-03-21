e088957215bf00a5cc948757e631bf00cfda9f2d
alemenew <alemenewendawek@gmail.com>
2022-03-22 02:11:09 +0300

    even

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
+ * puts2 - print only even index
  *
- *
- *
- *
+ * @str: hold string
  */
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; i <= str[i];  i++)
+	for (i = 0; str[i] != '\0';  i++)
 	{
 		if (str[i] % 2 == 0)
 			_putchar(str[i]);
