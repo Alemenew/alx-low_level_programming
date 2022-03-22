a199b5174d3e30eebe7152dc5ef446a22853b942
alemenew <alemenewendawek@gmail.com>
2022-03-22 13:14:21 +0300

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,18 +11,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i, j;
+	int i = -1;
 
-	i = 0;
-	while (src[i] != '\0')
-	{
+	do {
 		i++;
-	}
-
-	for (j = 0; j <= i; j++)
-	{
-		*(dest + j) = *(src + j);
-	}
-
+		dest[i] = src[i];
+	} while (src[i] != '\0');
 	return (dest);
+
 }
