7760472c34219f90b9bd8c1a73a17461a1fb5586
alemenew <alemenewendawek@gmail.com>
2022-03-24 20:10:26 +0300

    revers array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,11 +12,11 @@ void reverse_array(int *a, int n)
 	int i, tmp;
 
 	i = 0;
-	while (i <= n-1)
+	while (i <= n - 1)
 	{
 		tmp = a[i];
-		a[i] = a[n -1];
-		a[n -1] = tmp;
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
 		i++;
 		n--;
 	}
