a7c78b22f1883ee9903470f0de7959bf60ca70b0
alemenew <alemenewendawek@gmail.com>
2022-03-28 19:22:17 +0300

    length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,18 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i,j,n;
+	int i, j, n;
 	int c = 0;
-	for (i =0; s[i] != '\0'; i++)
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		n = 0;
-		for(j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if(s[i] == accept[j])
+			if (s[i] == accept[j])
 			{
 				c++;
-				n = n+1;
+				n = n + 1;
 			}
 		}
 		if (n == 0)
