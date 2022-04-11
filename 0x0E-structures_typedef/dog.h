d8cbdbfecfcdd80a51dfc788af07a9eca4087840
alemenew <alemenewendawek@gmail.com>
2022-04-11 18:29:38 +0300

    stract dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - holds varable
+ * @name: name
+ * @age: age of the user
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
