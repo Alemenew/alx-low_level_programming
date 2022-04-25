4f69591ad2dbb1ba0fb15d8ec38a53101e312d8c
alemenew <alemenewendawek@gmail.com>
2022-04-25 14:01:20 +0300

    printing node elements

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -1,33 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include <string.h>
-#include <string.h>
 #include "lists.h"
-
 
 /**
  * main - check the code
  *
- * Return: Always 0;
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	listint_t *head;
-	listint_t *new;
-	listint_ hello = {8, NULL};
-	size_t n;
+    listint_t *head;
+    listint_t *new;
+    listint_t hello = {8, NULL};
+    size_t n;
 
-	head = &hello;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	new->n = 9;
-	new->next = head;
-	n = print_list(head);
-	printf("-> %lu elemenets\n", n);
-	free(new);
-	return (0);
+    head = &hello;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->n = 9;
+    new->next = head;
+    head = new;
+    n = print_listint(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (0);
 }
--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,9 +15,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		printf("d\n" h->n);
+		printf("%d\n", h->n);
 		s++;
-		h->next;
+		h = h->next;
 	}
 	return (s);
 }
