21416651fce7e9926af0137db92e736d91f6dd8f
alemenew <alemenewendawek@gmail.com>
2022-04-29 00:34:55 +0300

    binary of number

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,5 +2,7 @@
 #define HEADER_FILE
 
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
+unsigned long int _pow(unsigned int base, unsigned int power);
+int _putchar(char c);
 #endif
