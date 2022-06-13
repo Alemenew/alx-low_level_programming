efff8b77dbc9ca95ddff8dcab038e4f24376d04a
alemenew <alemenewendawek@gmail.com>
2022-06-14 00:27:24 +0300

    libraries what have we

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Alemenew/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -O /tmp/win.so https://github.com/Alemenew/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
